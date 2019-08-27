7984eac59bf761e2e1760ccb3f2dafcf04c0aef0
Benjamin Ryan Mendoza <brb.mendoza@gmail.com>
2019-08-27 20:52:08 +0800

    Add mock API

---
+++ src/api/index.ts
@@ -0,0 +1,2 @@
+import * as UserAPI from './user';
+
---
+++ src/api/user/index.ts
@@ -0,0 +1,33 @@
+import { User } from '@/types/types';
+
+const register = (user: Omit<User, 'id'>): Promise<User> => {
+  // TODO: API 
+  const newId = Math.floor(Math.random() * 100 + 1);
+
+  return new Promise((resolve, reject) => {
+    if (newId >= 20) {
+      resolve({...user, id: newId});
+    } else {
+      reject('error while registering');
+    }
+  });
+}
+
+const login = (username: string, password: string): Promise<User> => {
+  // TODO: API
+  const hcUser = 'user';
+  const hcPassword = '123456';
+
+  return new Promise((resolve, reject) => {
+    if (username === hcUser && password === hcPassword) {
+      resolve({username, id: 0});
+    } else {
+      reject('error while logging in');
+    }
+  });
+}
+
+export const UserAPI = {
+  login,
+  register,
+};
